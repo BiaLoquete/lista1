4abd2635752d18858e6f281bae3ce440b920e0c6
BiaLoquete <124930485+BiaLoquete@users.noreply.github.com>
2023-03-02 22:56:32 -0300

    Add files via upload

--- lista1/ex6.cpp
+++ lista1/ex6.cpp
@@ -2,5 +2,26 @@
 
 using namespace std;
 
-main(){
-	setlocale(LC_ALL, "Portuguese");
+int main() {
+	setlocale (LC_ALL, "Portuguese");
+    int num, soma = 0;
+
+    cout << "Digite um número inteiro de três dígitos: ";
+    cin >> num;
+
+    if (num < 100 || num > 999) {
+        cout << "Número inválido" << endl;
+        return 0;
+    }
+
+    soma += num % 10;
+    num /= 10;
+    soma += num % 10;
+    num /= 10;
+    soma += num;
+
+    cout << "A soma dos dígitos do número é: " << soma << endl;
+
+    return 0;
+}
+
